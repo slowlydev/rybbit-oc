550054b59c9d40ffa821427e11d75e7158146903
Edward Cui <37892357+ECuiDev@users.noreply.github.com>
2025-11-04 01:33:31 -0800

    Fix reset bug

--- client/src/components/SiteSettings/ImportManager.tsx
+++ client/src/components/SiteSettings/ImportManager.tsx
@@ -127,6 +127,7 @@ export function ImportManager({ siteId, disabled }: ImportManagerProps) {
     mode: "onChange",
     defaultValues: {
       platform: "" as "umami",
+      file: Object.assign(new DataTransfer().files, {}),
       dateRange: {},
     },
   });
